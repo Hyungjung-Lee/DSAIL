1f61129b95f2b7d33e3af75a98757554ca132b3a
hyungjung.lee <lhj5772@me.com>
2023-07-19 21:22:38 +0900

    feat: add tf-idf

--- chapter2/topic_modeling.py
+++ chapter2/topic_modeling.py
@@ -25,4 +25,4 @@ transformed = cv.fit_transform(cleand)
 nmf = NMF(n_components=100, random_state=43).fit(transformed)
 for topic_idx, topic in enumerate(nmf.components_):
     label = f'{topic_idx} :'
-    print(label, " ".join([cv.get_feature_names()[i] for i in topic.argsort()[::-1]]))
+    print(label, " ".join([cv.get_feature_names()[i] for i in topic.argsort()[:-9:-1]]))
